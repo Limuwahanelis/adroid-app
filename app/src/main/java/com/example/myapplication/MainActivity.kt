4f11195cb2d3da4bffd513b7ee89885cfddfc939
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-15 23:31:51 +0100

    Add spacing between ui elements

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,18 +5,28 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,19 +46,37 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun UnitConverter()
-{
-    Column{
+fun UnitConverter() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
         // UI elements will be stacked below each other
         Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "Enter", onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp)) // paddiong is for single element for more precise control
         Row {
-            val context:Context = LocalContext.current;
-            // UI elements will be stacked next to each other
-            Button(onClick = { Toast.makeText(context,"Thanks for click",Toast.LENGTH_LONG).show()}) {
-                Text(text = "Click Me!!!!")
+            // dropdown needs a parent
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
             }
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+            }
+            // UI elements will be stacked next to each other
+
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Result: ")
     }
 }
 
@@ -59,3 +87,21 @@ fun UnitConverterPreview() {
         //Greeting("Android")
 
 }
+
+
+//@Composable
+//fun UnitConverter()
+//{
+//    Column{
+//        // UI elements will be stacked below each other
+//        Text("Unit Converter")
+//        OutlinedTextField(value = "Enter", onValueChange = {})
+//        Row {
+//            val context:Context = LocalContext.current;
+//            // UI elements will be stacked next to each other
+//            Button(onClick = { Toast.makeText(context,"Thanks for click",Toast.LENGTH_LONG).show()}) {
+//                Text(text = "Click Me!!!!")
+//            }
+//        }
+//    }
+//}
