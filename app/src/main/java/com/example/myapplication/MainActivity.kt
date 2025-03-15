e0ae0a754c420bd159349ee5f5b772babd30154e
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-15 19:27:35 +0100

    Add button with a toast.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
@@ -39,7 +43,11 @@ fun UnitConverter()
         Text("Unit Converter")
         OutlinedTextField(value = "Enter", onValueChange = {})
         Row {
+            val context:Context = LocalContext.current;
             // UI elements will be stacked next to each other
+            Button(onClick = { Toast.makeText(context,"Thanks for click",Toast.LENGTH_LONG).show()}) {
+                Text(text = "Click Me!!!!")
+            }
         }
     }
 }
