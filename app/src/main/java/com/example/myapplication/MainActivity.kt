7bd94431eace7f749e5c6e95f13bbdf839b15974
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2025-03-15 19:18:38 +0100

    Add text field

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,25 +25,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    UnitConverter();
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UnitConverter()
+{
+    Column{
+        // UI elements will be stacked below each other
+        Text("Unit Converter")
+        OutlinedTextField(value = "Enter", onValueChange = {})
+        Row {
+            // UI elements will be stacked next to each other
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true) // allows of checking how it looks after clicking split, without launching emulator
 @Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
+fun UnitConverterPreview() {
+        UnitConverter();
+        //Greeting("Android")
+
 }
